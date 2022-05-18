6c77302b357e71620307bbaa3267a60d9d1f129f
NYUMom <chrislo.cs27@gmail.com>
2022-05-17 23:07:53 -0400

    Responsive Design - Multiple Devices

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import {BsLinkedin} from 'react-icons/bs'
 import {FaGithub} from 'react-icons/fa'
+import {FaTwitter} from 'react-icons/fa'
+
 
 
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
         <a href='https://linkedin.com/in/christina-slocumb' target='_blank'><BsLinkedin /></a>
-        <a href='https://github.com' target='_blank'><FaGithub /></a>
-        <a href='https://dribble.com' target='_blank'></a>
+        <a href='https://github.com/NYUMom' target='_blank'><FaGithub /></a>
+        <a href='https://twitter.com/NYUBossMom' target='_blank'><FaTwitter /></a>
 
     </div>
   )
