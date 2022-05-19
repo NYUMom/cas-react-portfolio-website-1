7b444520fe1ecea6dc5accedd7487b470938ffd7
NYUMom <chrislo.cs27@gmail.com>
2022-05-18 23:23:32 -0400

    More Header Corrections

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,7 +9,9 @@ const Header = () => {
   return (
     <header>
       <div className="container header__container">
-        <h5>Hello I'm</h5>
+        <h5>Hi there</h5>
+        <h5>I'm</h5>
+
         <h1> Christina Slocumb</h1>
         <h5 className="text-light">FullStack Developer</h5>
         <CTA />
